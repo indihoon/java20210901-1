b1e7f395e637349ff5393843dab90bebdbe2c8fa
sebaek <sebaek@gmail.com>
2021-09-07 10:16:44 +0900

    anonymous class

---
+++ src/p08interface/p03lecture/p06anonymous_class/Example1.java
@@ -0,0 +1,13 @@
+package p08interface.p03lecture.p06anonymous_class;
+
+public class Example1 {
+	public static void main(String[] args) {
+//		MyInterface1 i1 = new MyInterface1(); // x
+		MyInterface1 i2 = new MyClass1(); // o
+		
+		// 클래스와 인스턴스(객체)를 한 번에 만들 수 있음
+		MyInterface1 i3 = new MyInterface1() {
+			
+		}; // o
+	}
+}
