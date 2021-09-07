61c45d8ae135c119f26aa6ee08f00a672f04f266
sebaek <sebaek@gmail.com>
2021-09-07 14:26:45 +0900

    scope

---
+++ src/p10exception/p03lecture/p02try_catch/Example3TryCatchScope.java
@@ -0,0 +1,21 @@
+package p10exception.p03lecture.p02try_catch;
+
+public class Example3TryCatchScope {
+	public static void main(String[] args) {
+		System.out.println("프로그램 시작");
+		
+		String str = "1aadfadfafdafadf00";
+		
+		int n = 0;
+		
+		try {
+			n = Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			System.out.println("넘버포맷익셉션 발생");
+		}
+		
+		System.out.println(n * 2);
+		
+		System.out.println("프로그램 종료");
+	}
+}
