c936b3a8bd4b44c3cd8e2df8ef5db15d40d3f56f
sebaek <sebaek@gmail.com>
2021-09-08 11:46:28 +0900

    polymorphism

--- src/p10exception/p03lecture/p06throws/Example8.java
+++ src/p10exception/p03lecture/p06throws/Example8.java
@@ -4,7 +4,20 @@ import java.io.IOException;
 
 public class Example8 {
 	public static void main(String[] args) {
+	
+		try {
+			method1();
+		} catch (IOException e) {
+//			e.printStackTrace();
+		}
 		
+		try {
+			method2();
+		} catch (Exception e) {
+//			e.printStackTrace();
+			System.out.println(e instanceof Exception);
+			System.out.println(e instanceof IOException);
+		}
 	}
 	
 	public static void method1() throws IOException {
