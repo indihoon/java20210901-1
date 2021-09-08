78b39d35547edac599bf0c6755e8b3c29562e06c
sebaek <sebaek@gmail.com>
2021-09-08 09:29:05 +0900

    checked, unchecked exception

---
+++ src/p10exception/p03lecture/p05throw/Example1ThrowRuntimeException.java
@@ -0,0 +1,17 @@
+package p10exception.p03lecture.p05throw;
+
+public class Example1ThrowRuntimeException {
+	public static void main(String[] args) {
+		
+		System.out.println("프로그램 시작");
+		
+		// RuntimeException을 상속 받은 Exception은
+		// try/catch를 사용해도 되고 안해도 됨
+		
+		NullPointerException e = new NullPointerException();
+		
+		throw e;
+		
+//		System.out.println("프로그램 종료");
+	}
+}
