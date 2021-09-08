d2c7e1b7c15e7f57218efb480587b9ba5f92104a
sebaek <sebaek@gmail.com>
2021-09-08 10:41:52 +0900

    ioexception

---
+++ src/p10exception/p02quiz/Example8.java
@@ -0,0 +1,14 @@
+package p10exception.p02quiz;
+
+import java.io.InputStreamReader;
+
+public class Example8 {
+
+	public static void main(String[] args) {
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		
+		isr.close();
+		
+	}
+}
