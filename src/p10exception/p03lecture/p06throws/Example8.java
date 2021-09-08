31aa5061f246f7c6234e397eeca2bc7aacdbe339
sebaek <sebaek@gmail.com>
2021-09-08 11:42:27 +0900

    exception polymorphism

---
+++ src/p10exception/p03lecture/p06throws/Example8.java
@@ -0,0 +1,17 @@
+package p10exception.p03lecture.p06throws;
+
+import java.io.IOException;
+
+public class Example8 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void method1() throws IOException {
+		throw new IOException();
+	}
+	
+	public static void method2() throws Exception {
+		throw new IOException();
+	}
+}
