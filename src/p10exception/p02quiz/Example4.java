8abe342966997f4dfc63921afdff6c7df98d7bd3
sebaek <sebaek@gmail.com>
2021-09-08 09:34:31 +0900

    throw exception

---
+++ src/p10exception/p02quiz/Example4.java
@@ -0,0 +1,5 @@
+package p10exception.p02quiz;
+
+public class Example4 {
+	// NullPointerException 발생 시키는 코드 작성
+}
---
+++ src/p10exception/p02quiz/Example6.java
@@ -0,0 +1,5 @@
+package p10exception.p02quiz;
+
+public class Example6 {
+	// ClassCastException
+}
