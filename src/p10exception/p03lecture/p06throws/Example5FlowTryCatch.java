65f0797ffa57ada1ba8ce8d870b127fd406d8d90
sebaek <sebaek@gmail.com>
2021-09-08 11:36:10 +0900

    flow

--- src/p10exception/p03lecture/p06throws/Example5FlowTryCatch.java
+++ src/p10exception/p03lecture/p06throws/Example5FlowTryCatch.java
@@ -1,5 +1,20 @@
 package p10exception.p03lecture.p06throws;
 
 public class Example5FlowTryCatch {
+	public static void main(String[] args) {
+		System.out.println("프로그램 시작");
 
+		try {
+			method1();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("프로그램 종료");
+	}
+
+	public static void method1() throws Exception {
+		throw new Exception();
+	}
 }
