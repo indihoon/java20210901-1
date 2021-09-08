907fca155ceb9a8c13fdcaeb10a4f566ffcfff54
sebaek <sebaek@gmail.com>
2021-09-08 10:04:39 +0900

    solution 4, 5, 6

--- src/p10exception/p02quiz/Example5.java
+++ src/p10exception/p02quiz/Example5.java
@@ -2,4 +2,12 @@ package p10exception.p02quiz;
 
 public class Example5 {
 	// ArrayIndexOutOfBoundsException
+	
+	public static void main(String[] args) {
+		int[] arr = {3,3};
+		
+		System.out.println(arr[1]);
+		
+		throw new ArrayIndexOutOfBoundsException();
+	}
 }
--- src/p10exception/p02quiz/Example6.java
+++ src/p10exception/p02quiz/Example6.java
@@ -2,4 +2,15 @@ package p10exception.p02quiz;
 
 public class Example6 {
 	// ClassCastException
+	
+	public static void main(String[] args) {
+		Object o = "java";
+		
+		String s = (String) o;
+//		Number n = (Number) o;
+		
+		System.out.println("프로그램 종료");
+		
+		throw new ClassCastException();
+	}
 }
