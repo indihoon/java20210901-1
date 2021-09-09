e03016ce355f0243b72830316bd8ab6a0ee0bd85
sebaek <sebaek@gmail.com>
2021-09-09 11:06:58 +0900

    indexof

--- src/p11api/p02quiz/Ex02IndexOf.java
+++ src/p11api/p02quiz/Ex02IndexOf.java
@@ -5,6 +5,51 @@ public class Ex02IndexOf {
 		String str = "간장 공장 공장장은 강 공장장이고 된장 공장 공장장은 공 공장장이다.";
 		
 		// "공장"이 있는 index들을 출력
+		String s = "공장";
+		int i = 0 - s.length();
+		
+		boolean run = true;
+		while (run) {
+			i = str.indexOf(s, i + s.length());
+			
+			if (i == -1) {
+				break;
+			}
+			
+			System.out.println(i);
+		}
+		
+		/*
+		int i1 = str.indexOf(s, 0);
+		
+		System.out.println(i1);
+		
+		int i2 = str.indexOf(s, i1 + s.length());
+		
+		System.out.println(i2);
+		
+		int i3 = str.indexOf(s, i2 + s.length());
+		
+		System.out.println(i3);
+		*/
+		
 		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
