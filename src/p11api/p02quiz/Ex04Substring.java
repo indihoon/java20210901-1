ae9b2f41bb67c548c402f8a49948097865853002
sebaek <sebaek@gmail.com>
2021-09-09 14:09:27 +0900

    substring

--- src/p11api/p02quiz/Ex04Substring.java
+++ src/p11api/p02quiz/Ex04Substring.java
@@ -2,6 +2,26 @@ package p11api.p02quiz;
 
 public class Ex04Substring {
 	public static void main(String[] args) {
+		String s1 = "spring";
+		int len1 = s1.length(); // 길이는 6
+		int c1 = len1 / 2; // 3
 		
+		String f1 = s1.substring(0, c1); // 0부터 c1-1까지 --> 0~2
+		String r1 = s1.substring(c1); // c1부터 끝까지 --> 3~5
+		
+		System.out.println(f1);
+		System.out.println(r1);
+		
+		System.out.println("=======================");
+		
+		String s2 = "apple";
+		int len2 = s2.length(); // 길이 5
+		int c2 = len2 / 2; // 2
+		
+		String f2 = s2.substring(0, c2); // 0부터 c2-1까지 --> 0~1
+		String r2 = s2.substring(c2); // c2부터 끝까지 --> 2~4
+		
+		System.out.println(f2);
+		System.out.println(r2);
 	}
 }
