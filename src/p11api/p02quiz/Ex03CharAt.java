9461b94c95d55768c83453f96c222245e92af8b7
sebaek <sebaek@gmail.com>
2021-09-09 11:37:58 +0900

    charat

--- src/p11api/p02quiz/Ex03CharAt.java
+++ src/p11api/p02quiz/Ex03CharAt.java
@@ -3,5 +3,21 @@ package p11api.p02quiz;
 public class Ex03CharAt {
 	public static void main(String[] args) {
 		
+		/*
+		String s1 = "apple";
+		int len1 = s1.length();
+		int c1 = len1 / 2; // 2.5x 2o
+		System.out.println(s1.charAt(c1));
+		
+		String s2 = "홍길동";
+		int len2 = s2.length();
+		int c2 = len2 / 2;
+		System.out.println(s2.charAt(c2));
+		
+		String s3 = "spring";
+		int len3 = s3.length();
+		int c3 = len3 / 2;
+		System.out.println(s3.charAt(c3));
+		*/
 	}
 }
