037129d8ce238d462ead6e108c4298df84f369e8
sebaek <sebaek@gmail.com>
2021-09-09 16:34:03 +0900

    counthi

--- src/p99codingbat/String2.java
+++ src/p99codingbat/String2.java
@@ -1,6 +1,19 @@
 package p99codingbat;
 
 public class String2 {
+	public int countHi(String str) {
+		int count = 0;
+
+		for (int i = 0; i < str.length() - 1; i++) {
+			String sub = str.substring(i, i + 2);
+			if (sub.equals("hi")) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
 	public String doubleChar(String str) {
 
 		String result = "";
