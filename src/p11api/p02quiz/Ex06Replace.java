3a17bca257078151085536d0adac441f9ac15266
sebaek <sebaek@gmail.com>
2021-09-09 14:40:36 +0900

    replace quiz

---
+++ src/p11api/p02quiz/Ex06Replace.java
@@ -0,0 +1,16 @@
+package p11api.p02quiz;
+
+import java.util.Scanner;
+
+public class Ex06Replace {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("입력:");
+		String str = scanner.nextLine();
+		
+		System.out.println(str.trim());
+		
+		scanner.close();
+	}
+}
