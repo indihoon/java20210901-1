5e9ad99257cb5fba76398d4cdd91726bc6fa4964
sebaek <sebaek@gmail.com>
2021-09-09 10:05:22 +0900

    length

--- src/p11api/p02quiz/Ex01Length.java
+++ src/p11api/p02quiz/Ex01Length.java
@@ -1,8 +1,35 @@
 package p11api.p02quiz;
 
+import java.util.Scanner;
+
 public class Ex01Length {
 	// scanner
 	// loop
 	// 입력받은 문자열이 몇 글자인지 출력하는 코드
-	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean run = true;
+		
+		while (run) {
+			System.out.print("입력:");
+			String str = scanner.next();
+			
+			if (str.equals("quit")) {
+				break;
+			}
+			
+			System.out.println(str.length() + "글자 입니다.");
+		}
+		
+		System.out.println("프로그램 종료");
+		scanner.close();
+	}
 }
+
+
+
+
+
+
+
