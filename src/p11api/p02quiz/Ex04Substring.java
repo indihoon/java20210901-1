6e1fe56436efbe843fc620c670c990a8ac284fa2
sebaek <sebaek@gmail.com>
2021-09-09 14:16:44 +0900

    substring

--- src/p11api/p02quiz/Ex04Substring.java
+++ src/p11api/p02quiz/Ex04Substring.java
@@ -1,7 +1,26 @@
 package p11api.p02quiz;
 
+import java.util.Scanner;
+
 public class Ex04Substring {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while (true) {
+			System.out.print("입력:");
+			String str = scanner.next();
+			
+			if (str.equals("q")) {
+				break;
+			}
+			
+			System.out.println("앞절반:" + str.substring(0, str.length()/2));
+			System.out.println("뒤절반:" + str.substring(str.length()/2));
+		}
+		
+		scanner.close();
+		
+		/*
 		String s1 = "spring";
 		int len1 = s1.length(); // 길이는 6
 		int c1 = len1 / 2; // 3
@@ -23,5 +42,6 @@ public class Ex04Substring {
 		
 		System.out.println(f2);
 		System.out.println(r2);
+		*/
 	}
 }
