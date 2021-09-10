dd1673ff066c61c68591cdc93c8553bc6a3d7f4c
sebaek <sebaek@gmail.com>
2021-09-10 14:28:28 +0900

    hasbad

--- src/p99codingbat/String1.java
+++ src/p99codingbat/String1.java
@@ -1,6 +1,25 @@
 package p99codingbat;
 
 public class String1 {
+	public boolean hasBad(String str) {
+		  
+		  return java.util.regex.Pattern.matches(".?bad.*", str);
+		  /*
+		  String f1 = "";
+		  if (str.length() >= 3) {
+		    f1 = str.substring(0, 3);  
+		  }
+		  
+		  
+		  String f2 = "";
+		  if (str.length() >= 4) {
+		    f2 = str.substring(1, 4);  
+		  }
+		  
+		  return f1.equals("bad") || f2.equals("bad");
+		  */
+	}
+	
 	public boolean endsLy(String str) {
 		  
 		  return java.util.regex.Pattern.matches(".*ly", str);
