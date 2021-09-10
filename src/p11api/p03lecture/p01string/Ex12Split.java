22620e486f4de180e76b95e6d63d091ec60d34cd
sebaek <sebaek@gmail.com>
2021-09-10 09:19:54 +0900

    split regex

--- src/p11api/p03lecture/p01string/Ex12Split.java
+++ src/p11api/p03lecture/p01string/Ex12Split.java
@@ -9,5 +9,40 @@ public class Ex12Split {
 		for (String item : arr) {
 			System.out.println(item);
 		}
+		
+		System.out.println("===================================");
+		
+		String str2 = "java spring html  css";
+		String[] arr2 = str2.split(" ");
+		
+		for (String item : arr2) {
+			System.out.println(item);
+		}
+		
+		System.out.println(arr2.length);
+		
+		System.out.println("===================================");
+		
+		String str3 = "java spring html  css";
+		String[] arr3 = str3.split(" +");
+		
+		for (String item : arr3) {
+			System.out.println(item);
+		}
+		System.out.println(arr3.length);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
