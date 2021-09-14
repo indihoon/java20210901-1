54cad7997cd91379d467550faddbd577795a10fd
sebaek <sebaek@gmail.com>
2021-09-14 14:28:34 +0900

    anonymous type runnable

--- src/p12thread/p03lecture/p02runnable/Ex03.java
+++ src/p12thread/p03lecture/p02runnable/Ex03.java
@@ -1,5 +1,7 @@
 package p12thread.p03lecture.p02runnable;
 
+import java.awt.Toolkit;
+
 public class Ex03 {
 	public static void main(String[] args) {
 		Runnable r1 = new Runnable() {
@@ -17,7 +19,24 @@ public class Ex03 {
 		};
 		
 		// 코드 작성 ...
-		
+		Runnable r2 = new Runnable() {
+			
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				
+				for (int i = 0; i < 5; i++) {
+					toolkit.beep();
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		};
 		
 		Thread t1 = new Thread(r1);
 		t1.start(); // 숫자가 1초에 한번씩 출력
