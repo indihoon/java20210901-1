f1d9be8bf0678e6b6b9d98acd47455ff01aec9a4
sebaek <sebaek@gmail.com>
2021-09-14 11:36:05 +0900

    single thread

---
+++ src/p12thread/p03lecture/Ex03.java
@@ -0,0 +1,26 @@
+package p12thread.p03lecture;
+
+import java.awt.Toolkit;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000); // 1000 milli second 만큼 멈추기
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/p12thread/p03lecture/Ex04.java
@@ -0,0 +1,7 @@
+package p12thread.p03lecture;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		
+	}
+}
