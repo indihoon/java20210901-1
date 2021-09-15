bdcd3858acd606393fbb21d47d31e831071d2fc5
sebaek <sebaek@gmail.com>
2021-09-15 14:07:50 +0900

    solution ex1

--- src/p15collection/p02quiz/Ex01.java
+++ src/p15collection/p02quiz/Ex01.java
@@ -1,5 +1,34 @@
 package p15collection.p02quiz;
 
-public class Ex01 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Ex01 {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		boolean run = true;
+		
+		while (run) {
+			System.out.print("l:list, e:exit 입력>");
+			String str = scanner.nextLine();
+			
+			switch (str) {
+			case "l":
+				for (String s : list) {
+					System.out.println(s);
+				}
+				break;
+			case "e":
+				run = false;
+				break;
+				
+			default:
+				list.add(str);
+				break;
+			}
+		}
+		
+		scanner.close();
+	}
 }
