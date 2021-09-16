fc664586298e8f65893fda2f1d09625756546ce2
sebaek <sebaek@gmail.com>
2021-09-16 10:19:34 +0900

    max

--- src/p15collection/p02quiz/Ex06.java
+++ src/p15collection/p02quiz/Ex06.java
@@ -6,6 +6,12 @@ public class Ex06 {
 	public int max(List<Integer> list) {
 		// list의 아이템 중 최대값 리턴
 		
-		return 0;
+		int max = list.get(0);
+		
+		for (int item : list) {
+			max = (max < item) ? item : max;
+		}
+		
+		return max;
 	}
 }
