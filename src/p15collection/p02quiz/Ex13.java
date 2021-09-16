0c6e8eb6124f7c18b9e52f4e738086ceb924e760
sebaek <sebaek@gmail.com>
2021-09-16 16:05:13 +0900

    ex12

---
+++ src/p15collection/p02quiz/Ex13.java
@@ -0,0 +1,19 @@
+package p15collection.p02quiz;
+
+import java.util.ArrayList;
+
+public class Ex13 {
+	public static void main(String[] args) {
+//		Math.random();
+		// 1~45사이의 값 6개 랜덤
+		ArrayList<Integer> lotto = new ArrayList<>();
+		
+		// 코드작성
+		
+		
+		
+		for (int l : lotto) {
+			System.out.println(l); // 6개 랜덤(중복 허용)
+		}
+	}
+}
