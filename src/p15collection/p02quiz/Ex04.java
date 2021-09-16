ee08e0cfcfbf469d3cd52bd6bb8af4a67f2cc779
sebaek <sebaek@gmail.com>
2021-09-16 09:37:55 +0900

    ex05

--- src/p15collection/p02quiz/Ex04.java
+++ src/p15collection/p02quiz/Ex04.java
@@ -4,7 +4,7 @@ public class Ex04 {
 	public int remainder(int num) {
 		// num을 10으로 나눈 나머지를 리턴
 		
-		return 0;
+		return num % 10;
 	}
 }
 // ctrl+n
---
+++ src/p15collection/p02quiz/Ex05.java
@@ -0,0 +1,10 @@
+package p15collection.p02quiz;
+
+public class Ex05 {
+	public int sum(String str) {
+		// str: "3 4"
+		// return: 7
+		
+		return 0;
+	}
+}
