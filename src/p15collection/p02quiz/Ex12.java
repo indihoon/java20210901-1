44a51aa83c070864086f269ed1c8fea2a2102219
sebaek <sebaek@gmail.com>
2021-09-16 14:08:49 +0900

    ex12

---
+++ src/p15collection/p02quiz/Ex12.java
@@ -0,0 +1,12 @@
+package p15collection.p02quiz;
+
+import java.util.List;
+
+public class Ex12 {
+	public boolean mirror(List<Integer> list) {
+		// list 중간 인덱스 기준으로 왼쪽과 오른쪽이 거울(데칼코마니)처럼 같은지
+		
+		
+		return false;
+	}
+}
