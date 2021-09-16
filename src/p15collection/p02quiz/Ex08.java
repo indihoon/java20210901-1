6ea6b0194a34154206031315dd1120af7bc7b14c
sebaek <sebaek@gmail.com>
2021-09-16 11:15:14 +0900

    maxindexlast

--- src/p15collection/p02quiz/Ex08.java
+++ src/p15collection/p02quiz/Ex08.java
@@ -6,6 +6,39 @@ public class Ex08 {
 	public int maxLastIndex(List<Integer> list) {
 		// list의 최대값이 있는 마지막 인덱스
 		
-		return 0;
+		int max = list.get(0);
+		int index = 0;
+		
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i) >= max) {
+				max = list.get(i);
+				index = i;
+			}
+		}
+		
+		return index;
+	}
+	
+	public int maxLastIndex2(List<Integer> list) {  
+		
+		int max = list.get(list.size() - 1);
+		int index = list.size() - 1;
+		
+		for (int i = list.size() - 2; i >= 0; i--) {
+			if (list.get(i) > max) {
+				max = list.get(i);
+				index = i;
+			}
+		}
+		
+		return index;
 	}
 }
+
+
+
+
+
+
+
+
--- src/p15collection/p02quiz/Ex08Test.java
+++ src/p15collection/p02quiz/Ex08Test.java
@@ -16,6 +16,10 @@ class Ex08Test {
 		assertEquals(5, e.maxLastIndex(Arrays.asList(-1, -2, -3, -100, -200, -1)));
 		assertEquals(3, e.maxLastIndex(Arrays.asList(10, 9, 8, 100, 1, 2, 3)));
 		
+		assertEquals(3, e.maxLastIndex2(Arrays.asList(8, 9, 10, 10, 9, 8)));
+		assertEquals(5, e.maxLastIndex2(Arrays.asList(-1, -2, -3, -100, -200, -1)));
+		assertEquals(3, e.maxLastIndex2(Arrays.asList(10, 9, 8, 100, 1, 2, 3)));
+		
 	}
 
 }
