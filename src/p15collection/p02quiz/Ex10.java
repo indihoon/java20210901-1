cf03950ec7581bbd07c1d57ffb237ba9abf21aaa
sebaek <sebaek@gmail.com>
2021-09-16 12:05:25 +0900

    evenlist

---
+++ src/p15collection/p02quiz/Ex10.java
@@ -0,0 +1,13 @@
+package p15collection.p02quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex10 {
+	public ArrayList<Integer> evenList(List<Integer> list) {
+		
+		// list의 짝수 아이템들만 모은 새 ArrayList를 만들어서 리턴
+		
+		return new ArrayList<>();
+	}
+}
