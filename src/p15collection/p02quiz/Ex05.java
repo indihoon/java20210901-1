45eb77d722f59762e6ff6d719bf5d0d240d5d729
sebaek <sebaek@gmail.com>
2021-09-16 09:47:08 +0900

    ex5

--- src/p15collection/p02quiz/Ex05.java
+++ src/p15collection/p02quiz/Ex05.java
@@ -5,6 +5,11 @@ public class Ex05 {
 		// str: "3 4"
 		// return: 7
 		
-		return 0;
+		String[] nums = str.split(" ");
+		
+		int num1 = Integer.parseInt(nums[0]);
+		int num2 = Integer.parseInt(nums[1]);
+		
+		return num1 + num2;
 	}
 }
