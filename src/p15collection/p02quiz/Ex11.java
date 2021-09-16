c6543f7b88fefa3e1e98535b0e221897451bb83d
sebaek <sebaek@gmail.com>
2021-09-16 15:06:50 +0900

    minmax

--- src/p15collection/p02quiz/Ex11.java
+++ src/p15collection/p02quiz/Ex11.java
@@ -7,8 +7,19 @@ public class Ex11 {
 	public ArrayList<Integer> minMax(List<Integer> list) {
 		
 		// 파라미터로 받은 list의 최소값, 최대값을 아이템으로 갖는 ArrayList 리턴
+		ArrayList<Integer> result = new ArrayList<>();
 		
+		int min = list.get(0);
+		int max = list.get(1);
 		
-		return new ArrayList<>();
+		for (int item : list) {
+			min = Math.min(min, item);
+			max = Math.max(max, item);
+		}
+		
+		result.add(min);
+		result.add(max);
+		
+		return result;
 	}
 }
