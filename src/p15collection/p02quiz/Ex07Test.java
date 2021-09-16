a3fc5b445e951eba46dc500962dd3a9408340900
sebaek <sebaek@gmail.com>
2021-09-16 10:30:02 +0900

    Update Ex07Test.java

--- src/p15collection/p02quiz/Ex07Test.java
+++ src/p15collection/p02quiz/Ex07Test.java
@@ -15,6 +15,7 @@ class Ex07Test {
 		assertEquals(2, e.maxIndex(Arrays.asList(33, 3, 100, -80)));
 		assertEquals(3, e.maxIndex(Arrays.asList(7, 8, 9, 10)));
 		assertEquals(3, e.maxIndex(Arrays.asList(-10, -8, -6, -1, -5)));
+		assertEquals(0, e.maxIndex(Arrays.asList(100, 50, 40, 100, 0)));
 	}
 
 }
