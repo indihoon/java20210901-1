d68d8093a53f5a37deb8fd50376eb696e47e5581
sebaek <sebaek@gmail.com>
2021-09-17 16:35:20 +0900

    diff

--- src/p15collection/p02quiz/p02set/Ex08.java
+++ src/p15collection/p02quiz/p02set/Ex08.java
@@ -1,12 +1,21 @@
 package p15collection.p02quiz.p02set;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Ex08 {
 	public Set<Integer> diff(int[] a, int[] b) {
 		// a배열에만 있는 아이템들만 모은 set 리턴
+		Set<Integer> set = new HashSet<>();
 		
+		for (int item : a) {
+			set.add(item);
+		}
 		
-		return null;
+		for (int item : b) {
+			set.remove(item);
+		}
+		
+		return set;
 	}
 }
