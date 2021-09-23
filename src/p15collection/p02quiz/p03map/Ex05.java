dec1f72e38c1f471293bf9648daf7061d2f9e36a
sebaek <sebaek@gmail.com>
2021-09-23 11:24:12 +0900

    ex05

---
+++ src/p15collection/p02quiz/p03map/Ex05.java
@@ -0,0 +1,14 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex05 {
+	public Map<Integer, Integer> doubling(Map<Integer, Integer> param) {
+		// param이 가지고 있는 value를 2배한 새 맵을 만들어서 return
+		// param이 변경되면 안됨
+		
+		
+		return null;
+	}
+
+}
