c7898cd67f8551835e6199e93e9d4e6fffc8e25b
sebaek <sebaek@gmail.com>
2021-09-23 12:24:47 +0900

    quiz swao

---
+++ src/p15collection/p02quiz/p03map/Ex07.java
@@ -0,0 +1,15 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex07 {
+	public Map<String, String> swap(Map<String, String> param) {
+		// param의 size가 2
+		// 두 value를 서로 바꾼(swap) map을 만들어서 return
+		// param은 변경 x
+		
+		
+		
+		return null;
+	}
+}
