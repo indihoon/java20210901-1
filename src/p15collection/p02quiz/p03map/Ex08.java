563201cced540c02cd5d40baa2d9575843920b9f
sebaek <sebaek@gmail.com>
2021-09-23 12:39:18 +0900

    quiz swap 8

---
+++ src/p15collection/p02quiz/p03map/Ex08.java
@@ -0,0 +1,13 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex08 {
+	public void swap(Map<String, String> param) {
+		// param size : 2
+		// param 키는 "a", "b"
+		// param 각 값을 변경(swap)
+		
+		
+	}
+}
