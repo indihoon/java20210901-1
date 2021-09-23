57e3c4e5013c81801d6f2bec5dcee204200df542
sebaek <sebaek@gmail.com>
2021-09-23 11:39:21 +0900

    sol ex5

--- src/p15collection/p02quiz/p03map/Ex05.java
+++ src/p15collection/p02quiz/p03map/Ex05.java
@@ -1,14 +1,23 @@
 package p15collection.p02quiz.p03map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Ex05 {
 	public Map<Integer, Integer> doubling(Map<Integer, Integer> param) {
 		// param이 가지고 있는 value를 2배한 새 맵을 만들어서 return
 		// param이 변경되면 안됨
+		Map<Integer, Integer> map = new HashMap();
 		
+		Set<Integer> keys = param.keySet();
+		for (Integer key : keys) {
+			Integer val = param.get(key);
+			
+			map.put(key, val * 2);
+		}
 		
-		return null;
+		return map;
 	}
 
 }
