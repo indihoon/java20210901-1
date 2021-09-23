ce529a2ac8d121dd9d661eeb7059c602988c4889
sebaek <sebaek@gmail.com>
2021-09-23 16:05:59 +0900

    sol q10

--- src/p15collection/p02quiz/p03map/Ex10.java
+++ src/p15collection/p02quiz/p03map/Ex10.java
@@ -1,6 +1,8 @@
 package p15collection.p02quiz.p03map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Ex10 {
 	public Map<String, Integer> sum(Map<String, Integer> map1, Map<String, Integer> map2) {
@@ -8,6 +10,36 @@ public class Ex10 {
 		// 한 쪽에만 있으면 그냥 추가
 		// 한 새 map을 만들어서 리턴
 		
-		return null;
+		Map<String, Integer> res = new HashMap<>();
+		
+		// map1의 entry들을 res에 옮김
+		Set<String> keys1 = map1.keySet();
+		for (String key : keys1) {
+			res.put(key, map1.get(key));
+		}
+		
+		
+		Set<String> keys2 = map2.keySet();
+		for (String key : keys2) {
+			if (res.containsKey(key)) {
+				Integer v = res.get(key);
+				res.put(key, v + map2.get(key));
+			} else {
+				res.put(key, map2.get(key));
+			}
+		}
+		
+		return res;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
