a4de0f614d886481302c8d991a1bb1258c93b796
sebaek <sebaek@gmail.com>
2021-09-23 10:29:14 +0900

    sol q2

--- src/p15collection/p02quiz/p03map/Ex02.java
+++ src/p15collection/p02quiz/p03map/Ex02.java
@@ -1,5 +1,6 @@
 package p15collection.p02quiz.p03map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Ex02 {
@@ -7,6 +8,10 @@ public class Ex02 {
 		// map을 만들어서 return
 		// 1:2, 2:4 추가
 		
-		return null;
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(1, 2);
+		map.put(2, 4);
+		
+		return map;
 	}
 }
