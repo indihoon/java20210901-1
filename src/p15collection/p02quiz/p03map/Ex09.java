895c2e2f40d9cba809af3afdb6b21207f81363d1
sebaek <sebaek@gmail.com>
2021-09-23 14:21:29 +0900

    cound char

---
+++ src/p15collection/p02quiz/p03map/Ex09.java
@@ -0,0 +1,12 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex09 {
+	public Map<Character, Integer> countCharacter(String str) {
+		// str의 문자가 몇번 나오는지 세어서
+		// 문자를 key 몇번을 value 로 갖는 map 만들어서 return
+		
+		return null;
+	}
+}
