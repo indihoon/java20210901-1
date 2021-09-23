8061b950989d4720594395a1b85d9ea1fc9bbeea
sebaek <sebaek@gmail.com>
2021-09-23 15:36:54 +0900

    quiz sum

---
+++ src/p15collection/p02quiz/p03map/Ex10.java
@@ -0,0 +1,13 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex10 {
+	public Map<String, Integer> sum(Map<String, Integer> map1, Map<String, Integer> map2) {
+		// map1과 map2에 키가 둘 다 있으면 밸류를 더해서 추가
+		// 한 쪽에만 있으면 그냥 추가
+		// 한 새 map을 만들어서 리턴
+		
+		return null;
+	}
+}
