06a7d573654b26a03c5d531c289f22823a56d259
sebaek <sebaek@gmail.com>
2021-09-23 10:35:01 +0900

    quiz3

---
+++ src/p15collection/p02quiz/p03map/Ex03.java
@@ -0,0 +1,16 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex03 {
+	public Map<Integer, Integer> triple() {
+		// map을 만들어서 return
+		// 키:밸류  총 5개
+		// 키 : 1~5
+		// 밸류 : 키 * 3
+		
+		
+		return null;
+	}
+}
+
