48d56aa69b489084c3199a2921aaf9e3e563027e
sebaek <sebaek@gmail.com>
2021-09-23 10:22:48 +0900

    quiz2

---
+++ src/p15collection/p02quiz/p03map/Ex02.java
@@ -0,0 +1,12 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex02 {
+	public Map<Integer, Integer> put() {
+		// map을 만들어서 return
+		// 1:2, 2:4 추가
+		
+		return null;
+	}
+}
