d8e4b6d5a5c797095f472e37a9bbf29077b1aa54
sebaek <sebaek@gmail.com>
2021-09-23 12:04:33 +0900

    quiz 6

---
+++ src/p15collection/p02quiz/p03map/Ex06.java
@@ -0,0 +1,12 @@
+package p15collection.p02quiz.p03map;
+
+import java.util.Map;
+
+public class Ex06 {
+
+	public void doubling(Map<Integer, Integer> map) {
+		// 파라미터 map의 각 값(value)을 2배로
+		
+		
+	}
+}
