d3de3b45c989e7a82d2e9a7ae39590f707fa5c15
sebaek <sebaek@gmail.com>
2021-09-23 15:06:24 +0900

    sol q9

--- src/p15collection/p02quiz/p03map/Ex09.java
+++ src/p15collection/p02quiz/p03map/Ex09.java
@@ -1,5 +1,6 @@
 package p15collection.p02quiz.p03map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Ex09 {
@@ -7,6 +8,19 @@ public class Ex09 {
 		// str의 문자가 몇번 나오는지 세어서
 		// 문자를 key 몇번을 value 로 갖는 map 만들어서 return
 		
-		return null;
+		Map<Character, Integer> result = new HashMap<>();
+		
+		for (int i = 0; i < str.length(); i++) {
+			Character c = str.charAt(i);
+			
+			if (result.containsKey(c)) {
+				Integer v = result.get(c);
+				result.put(c, v + 1);
+			} else {
+				result.put(c, 1);
+			}
+		}
+		
+		return result;
 	}
 }
