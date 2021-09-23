decf8c4af0d96eb5f4501a53cc76a618facd4f71
sebaek <sebaek@gmail.com>
2021-09-23 12:26:53 +0900

    Update Ex07.java

--- src/p15collection/p02quiz/p03map/Ex07.java
+++ src/p15collection/p02quiz/p03map/Ex07.java
@@ -5,9 +5,9 @@ import java.util.Map;
 public class Ex07 {
 	public Map<String, String> swap(Map<String, String> param) {
 		// param의 size가 2
+		// param의 키는 "a", "b"
 		// 두 value를 서로 바꾼(swap) map을 만들어서 return
 		// param은 변경 x
-		
 		
 		
 		return null;
