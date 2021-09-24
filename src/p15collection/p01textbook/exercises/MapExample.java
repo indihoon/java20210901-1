9a6733bd95a543c7e1dba574a66dd4403ca5fd2a
sebaek <sebaek@gmail.com>
2021-09-24 15:32:33 +0900

    exercise 9

---
+++ src/p15collection/p01textbook/exercises/MapExample.java
@@ -0,0 +1,19 @@
+package p15collection.p01textbook.exercises;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+		
+		// 작성 위치
+	}
+}
