3a3cc4fabf23c2539b4007f54e15bfeedf4185e6
sebaek <sebaek@gmail.com>
2021-09-27 14:31:37 +0900

    Update Ex01.java

--- src/p13generic/p03lecture/p04extends/Ex01.java
+++ src/p13generic/p03lecture/p04extends/Ex01.java
@@ -1,11 +1,16 @@
 package p13generic.p03lecture.p04extends;
 
 public class Ex01 {
-
+	public static void main(String[] args) {
+		Child3Type t3 = new Child3Type();
+		t3.method("hello");
+	}
 }	
 
 class MyType<T> {
-	
+	public void method(T param) {
+		
+	}
 }
 
 class ChildType<T> extends MyType<T> {
