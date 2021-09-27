c88aaaa6688863227e9bb0570495bcbd7793a64b
sebaek <sebaek@gmail.com>
2021-09-27 15:45:14 +0900

    sol q1

--- src/p14lambda/p02quiz/Ex02.java
+++ src/p14lambda/p02quiz/Ex02.java
@@ -2,8 +2,8 @@ package p14lambda.p02quiz;
 
 public class Ex02 {
 	public static void main(String[] args) {
-		MyInterface2 o1 = ; // 적절한 코드 작성
-		MyInterface2 o2 = ; // 적절한 코드 작성
+		MyInterface2 o1 = () -> System.out.println("hello"); // 적절한 코드 작성
+		MyInterface2 o2 = () -> System.out.println("java"); // 적절한 코드 작성
 		
 		o1.method(); // hello
 		o2.method(); // java
