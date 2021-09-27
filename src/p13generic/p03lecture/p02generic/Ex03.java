ba50cf7752f15f21c9d27996c3712612b326ed05
sebaek <sebaek@gmail.com>
2021-09-27 09:45:54 +0900

    generic method

--- src/p13generic/p03lecture/p02generic/Ex03.java
+++ src/p13generic/p03lecture/p02generic/Ex03.java
@@ -16,6 +16,11 @@ public class Ex03 {
 		
 		Ex03.<Double>method2(3.14);
 		Ex03.method2(3.14);
+		
+		System.out.println("=============");
+		Ex03.<Integer, Double>method3(300, 3.4);
+		Ex03.method3(900, 3.14);
+		
 	}
 	
 	public static <T> void method2(T param) {
@@ -23,4 +28,26 @@ public class Ex03 {
 		System.out.println(param instanceof Integer);
 		System.out.println(param instanceof Double);
 	}
+	
+	public static <T, U> void method3(T param1, U param2) {
+		System.out.println(param1);
+		System.out.println(param2);
+		
+		System.out.println(param1 instanceof Integer);
+		System.out.println(param1 instanceof Double);
+		
+		System.out.println(param2 instanceof Integer);
+		System.out.println(param2 instanceof Double);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
