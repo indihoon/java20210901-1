7ac005dd53ae9abc94ea893dce86a05eec7764af
sebaek <sebaek@gmail.com>
2021-09-27 15:22:31 +0900

    quiz1

---
+++ src/p14lambda/p02quiz/Ex01.java
@@ -0,0 +1,17 @@
+package p14lambda.p02quiz;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		MyInterface1 o1;
+		
+		// 코드 작성
+		
+		
+		o1.method();
+	}
+}
+
+
+interface MyInterface1 {
+	public void method();
+}
