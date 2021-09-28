acf4526c842c10fcda7f8ee4862df9cd87de9002
sebaek <sebaek@gmail.com>
2021-09-28 10:44:25 +0900

    map foreach

--- src/p14lambda/p02quiz/Ex06.java
+++ src/p14lambda/p02quiz/Ex06.java
@@ -9,7 +9,13 @@ public class Ex06 {
 		map.put("lang", "java");
 		map.put("fw", "spring");
 		map.put("fe", "html");
-		
+
+		/*
+		 * key:lang, value:java
+		 * key:fw, value:spring
+		 * key:fe, value:html
+		 */
 		// map의 foreach 활용
+		map.forEach((key, val) -> System.out.println("key:" + key + ", value:" + val));
 	}
 }
