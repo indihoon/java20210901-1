f1b66f76f5dd5e321522a55b822a991218676384
sebaek <sebaek@gmail.com>
2021-09-28 10:14:27 +0900

    sol quiz5

--- src/p14lambda/p02quiz/Ex05.java
+++ src/p14lambda/p02quiz/Ex05.java
@@ -8,6 +8,7 @@ public class Ex05 {
 		List<Integer> list = Arrays.asList(3, 4, 5, 6);
 		
 		// list.forEach 활용, lambda
+		list.forEach(item -> System.out.print((item*2) + " "));
 		
 		// 출력 결과 (한 줄에)
 		// 6 8 10 12 
