40d8982821d985b60cfa68c0c8e152b88ce4ce89
sebaek <sebaek@gmail.com>
2021-09-28 14:08:24 +0900

    sol q10

--- src/p14lambda/p02quiz/Ex10.java
+++ src/p14lambda/p02quiz/Ex10.java
@@ -7,11 +7,19 @@ import java.util.List;
 public class Ex10 {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>(Arrays.asList("java", "spring", "html", "a", "", "css"));
-		
+
 		// List.replaceAll
-		
-		
-		
+		list.replaceAll(s -> {
+
+			if (s.length() <= 2) {
+				return s;
+			}
+			return s.substring(s.length() - 2);
+		});
+
+		// 삼항연산식 활용
+		list.replaceAll(s -> s.length() <= 2 ? s : s.substring(s.length() - 2));
+
 		System.out.println(list); // [va, ng, ml, a, , ss]
 	}
 }
