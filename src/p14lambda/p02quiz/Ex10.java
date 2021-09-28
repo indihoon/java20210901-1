39806531d7dec76489f132915dfb01de7f9f1ae3
sebaek <sebaek@gmail.com>
2021-09-28 12:17:58 +0900

    quiz 10

---
+++ src/p14lambda/p02quiz/Ex10.java
@@ -0,0 +1,17 @@
+package p14lambda.p02quiz;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>(Arrays.asList("java", "spring", "html", "a", "", "css"));
+		
+		// List.replaceAll
+		
+		
+		
+		System.out.println(list); // [va, ng, ml, a, , ss]
+	}
+}
