6182ca854283392d13d0fa5e64ad81c1215bc26c
sebaek <sebaek@gmail.com>
2021-09-28 16:08:45 +0900

    map compute sol quiz13

--- src/p14lambda/p03lecture/Ex13MapCompute.java
+++ src/p14lambda/p03lecture/Ex13MapCompute.java
@@ -1,11 +1,17 @@
 package p14lambda.p03lecture;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Ex13MapCompute {
 	
 	public Map<Character, Integer> count(String str) {
+		Map<Character, Integer> res = new HashMap<>();
 		
-		return null;
+		for (Character c : str.toCharArray()) {
+			res.compute(c, (k, v) -> v == null ? 1 : v + 1);
+		}
+		
+		return res;
 	}
 }
