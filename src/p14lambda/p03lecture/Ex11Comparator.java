2642972622a1d5e05128e6db5838827d7b5df823
sebaek <sebaek@gmail.com>
2021-09-28 14:29:05 +0900

    comparator

--- src/p14lambda/p03lecture/Ex11Comparator.java
+++ src/p14lambda/p03lecture/Ex11Comparator.java
@@ -1,6 +1,7 @@
 package p14lambda.p03lecture;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Ex11Comparator {
@@ -24,6 +25,12 @@ public class Ex11Comparator {
 		System.out.println(arr);
 		
 		arr.sort((o1, o2) -> o2.compareTo(o1));
+		System.out.println(arr);
+		
+		arr.sort(Comparator.naturalOrder());
+		System.out.println(arr);
+		
+		arr.sort(Comparator.reverseOrder());
 		System.out.println(arr);
 	}
 }
