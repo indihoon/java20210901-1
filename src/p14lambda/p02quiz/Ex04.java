5d952f7fd57acc26ba72bc70e7586124e7304387
sebaek <sebaek@gmail.com>
2021-09-28 09:41:02 +0900

    sol q4

--- src/p14lambda/p02quiz/Ex04.java
+++ src/p14lambda/p02quiz/Ex04.java
@@ -7,8 +7,9 @@ public class Ex04 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(3, 4, 5, 6);
 		
-		method(list, ); // 코드 완성
-		method(list, ); // 코드 완성
+		method(list, num -> System.out.println(num)); // 코드 완성
+		method(list, num -> System.out.println("item : " + num)); // 코드 완성
+		method(list, n -> System.out.println("item : " + n*2)); // 코드 완성
 	}
 	
 	public static void method(List<Integer> list, MyInterface4 c) {
