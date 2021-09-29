ade3afcfeebf8a60b10abfe74da3bf22b396894a
sebaek <sebaek@gmail.com>
2021-09-29 12:36:23 +0900

    copy file

--- src/p18io/p02quiz/Ex04.java
+++ src/p18io/p02quiz/Ex04.java
@@ -2,9 +2,9 @@ package p18io.p02quiz;
 
 public class Ex04 {
 	public static void main(String[] args) {
-		// 각자 가진 그림 파일을 내용으로 채워진 새 파일 만들기
+		// 각자 가진 그림 파일 내용으로 채워진 새 파일 만들기
 		// 파일명 outtest2 (원본 파일 확장자 사용)
-		// 원본 파일과 같은 크기, 같은 내용
+		// 원본 파일과 같은 크기, 같은 내용 (파일 복사)
 		
 	}
 }
