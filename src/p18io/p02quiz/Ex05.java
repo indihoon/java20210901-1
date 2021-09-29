db2ca8cb2f594c28fa2dbe4c0cf348e1cc4c6eb3
sebaek <sebaek@gmail.com>
2021-09-29 14:24:05 +0900

    create file

---
+++ src/p18io/p02quiz/Ex05.java
@@ -0,0 +1,14 @@
+package p18io.p02quiz;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		// 파일명:outtest4.txt
+		// 파일크기:1024bytes
+		// 내용은 상관없음
+		// 파일 만들어보기
+		// OutputStream.write(byte[]) 사용해보기
+		
+		
+		
+	}
+}
