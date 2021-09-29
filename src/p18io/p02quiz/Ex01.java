2849a17bb0be8623c4157bd7d0408abb59a8e1a7
sebaek <sebaek@gmail.com>
2021-09-29 10:17:22 +0900

    read

--- src/p18io/p02quiz/Ex01.java
+++ src/p18io/p02quiz/Ex01.java
@@ -1,5 +1,19 @@
 package p18io.p02quiz;
 
-public class Ex01 {
+import java.io.FileInputStream;
+import java.io.InputStream;
 
+public class Ex01 {
+	public static void main(String[] args) throws Exception {
+		String file = "C:\\Users\\user\\Desktop\\iotest\\다운로드.jfif";
+		InputStream is = new FileInputStream(file);
+		int size = 0;
+		
+//		while () // InputStream.read() 를 사용해서 loop가 몇 번 실행되었는지 size에 저장 
+		
+		
+		System.out.println("파일크기:" + size); // 파일크기:6778
+		System.out.println("프로그램 종료");
+		is.close();
+	}
 }
