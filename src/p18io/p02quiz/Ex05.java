b6f7ef9757f824bc25d0c335c2404889c3006f60
sebaek <sebaek@gmail.com>
2021-09-29 14:25:53 +0900

    Update Ex05.java

--- src/p18io/p02quiz/Ex05.java
+++ src/p18io/p02quiz/Ex05.java
@@ -3,7 +3,7 @@ package p18io.p02quiz;
 public class Ex05 {
 	public static void main(String[] args) {
 		// 파일명:outtest4.txt
-		// 파일크기:1024bytes
+		// 파일크기:1Mbytes = 1024*1024bytes
 		// 내용은 상관없음
 		// 파일 만들어보기
 		// OutputStream.write(byte[]) 사용해보기
