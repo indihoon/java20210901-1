46b9612789624ce3425f4acc8320d90b3eec67d9
sebaek <sebaek@gmail.com>
2021-09-30 12:19:14 +0900

    Update Ex11.java

--- src/p18io/p02quiz/Ex11.java
+++ src/p18io/p02quiz/Ex11.java
@@ -4,6 +4,8 @@ public class Ex11 {
 	public static void main(String[] args) {
 		String name = "C:\\Users\\user\\Desktop\\iotest\\outtestC.txt";
 		
+		// 사용자로부터 입력 받은 내용을
+		// 파일에 작성하기
 		
 		
 	}
