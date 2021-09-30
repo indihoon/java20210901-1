aceffda3e7495f29ea28d059e7de7b4cd164f996
sebaek <sebaek@gmail.com>
2021-09-30 12:19:40 +0900

    Update Ex11.java

--- src/p18io/p02quiz/Ex11.java
+++ src/p18io/p02quiz/Ex11.java
@@ -2,7 +2,7 @@ package p18io.p02quiz;
 
 public class Ex11 {
 	public static void main(String[] args) {
-		String name = "C:\\Users\\user\\Desktop\\iotest\\outtestC.txt";
+		String name = "C:\\Users\\user\\Desktop\\iotest\\outtestD.txt";
 		
 		// 사용자로부터 입력 받은 내용을
 		// 파일에 작성하기
