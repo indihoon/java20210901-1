edc99d1814206dbc207a3d1cfae8ae288cf22f0f
sebaek <sebaek@gmail.com>
2021-09-30 10:23:35 +0900

    String constructor

--- src/p18io/p02quiz/Ex08Test.java
+++ src/p18io/p02quiz/Ex08Test.java
@@ -13,5 +13,14 @@ class Ex08Test {
 		assertEquals("a가,b나,c다,라마,바사", e.split(2));
 		assertEquals("a가b,나c다,라마바,사", e.split(3));
 	}
+	
+	@Test
+	void testStringConstructor() {
+		char[] arr1 = {'j', 'a', 'v', 'a'};
+		
+		assertEquals("java", new String(arr1));
+		assertEquals("ja", new String(arr1, 0, 2));
+		assertEquals("va", new String(arr1, 2, 2));
+	}
 
 }
