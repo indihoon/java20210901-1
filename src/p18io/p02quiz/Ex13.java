da8d41c9cfd2b6ac412db3a6bc0e0d67a29e2808
sebaek <sebaek@gmail.com>
2021-09-30 16:04:34 +0900

    quiz13

---
+++ src/p18io/p02quiz/Ex13.java
@@ -0,0 +1,12 @@
+package p18io.p02quiz;
+
+public class Ex13 {
+	public static void main(String[] args) {
+		String src = "src/p18io/p02quiz/Ex13.java";
+		String des = "C:\\Users\\user\\Desktop\\iotest\\Ex13.java";
+		
+		// src -> des
+		// 라인넘버를 앞에 붙여서
+		
+	}
+}
