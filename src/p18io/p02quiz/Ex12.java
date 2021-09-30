6aaf0d0bb0a93c18f764c4f2f4c31df7939d57bf
sebaek <sebaek@gmail.com>
2021-09-30 15:34:19 +0900

    quiz12

---
+++ src/p18io/p02quiz/Ex12.java
@@ -0,0 +1,11 @@
+package p18io.p02quiz;
+
+public class Ex12 {
+	public static void main(String[] args) {
+		String file = "src/p18io/p02quiz/Ex12.java";
+		
+		
+		// 이 파일을 출력 
+		// 앞에 라인 넘버 붙여서 출력
+	}
+}
