895650daa95c4604ae3c4dcd851db716441cfb82
sebaek <sebaek@gmail.com>
2021-10-01 10:02:48 +0900

    copy file

---
+++ src/p18io/p02quiz/Ex14.java
@@ -0,0 +1,10 @@
+package p18io.p02quiz;
+
+public class Ex14 {
+	public static void main(String[] args) {
+		String src = "C:\\Users\\user\\Desktop\\iotest\\outtestF.txt";
+		String des = "C:\\Users\\user\\Desktop\\iotest\\outtestG.txt";
+		
+		// 파일 복사 (BufferedInputStream, BufferedOutputStream 활용)
+	}
+}
