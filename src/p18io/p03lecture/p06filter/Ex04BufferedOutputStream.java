8cc233304700b37978de4a7d12ae1c3272edc228
sebaek <sebaek@gmail.com>
2021-10-01 09:21:48 +0900

    bufferedOutputStream

--- src/p18io/p03lecture/p06filter/Ex04BufferedOutputStream.java
+++ src/p18io/p03lecture/p06filter/Ex04BufferedOutputStream.java
@@ -1,5 +1,6 @@
 package p18io.p03lecture.p06filter;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
@@ -7,15 +8,18 @@ public class Ex04BufferedOutputStream {
 	public static void main(String[] args) throws Exception {
 		String file = "C:\\Users\\user\\Desktop\\iotest\\outtestF.txt";
 		OutputStream os = new FileOutputStream(file);
+		BufferedOutputStream bos = new BufferedOutputStream(os);
 		
 		long start = System.currentTimeMillis();
-		for (int i = 0; i < 1024 * 1024 * 500; i++) {
-			os.write('a');
+		for (int i = 0; i < 1024 * 1024 * 1; i++) {
+//			os.write('a'); //1789ms 1814ms
+			bos.write('a'); //21ms 23ms
 		}
 		long end = System.currentTimeMillis();
 		
 		System.out.println((end - start) + "ms");
 		
-		os.close();
+		bos.close();
+//		os.close();
 	}
 }
