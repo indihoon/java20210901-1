5557483cf6d813d7802b858834040dfa419f4064
sebaek <sebaek@gmail.com>
2021-10-01 09:38:08 +0900

    bufferedInputSTream

--- src/p18io/p03lecture/p06filter/Ex05BufferedInputStream.java
+++ src/p18io/p03lecture/p06filter/Ex05BufferedInputStream.java
@@ -1,5 +1,6 @@
 package p18io.p03lecture.p06filter;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
@@ -7,14 +8,18 @@ public class Ex05BufferedInputStream {
 	public static void main(String[] args) throws Exception {
 		String file = "C:\\Users\\user\\Desktop\\iotest\\outtestF.txt";
 		InputStream is = new FileInputStream(file);
+		BufferedInputStream bis = new BufferedInputStream(is);
 
 		long start = System.currentTimeMillis();
-		while (is.read() != -1) {//1346ms 1326ms
+//		while (is.read() != -1) {//1346ms 1326ms
+		while (bis.read() != -1) {// 21ms 20ms
 
 		}
 		long end = System.currentTimeMillis();
 
 		System.out.println((end - start) + "ms");
+		
+		bis.close();
 		is.close();
 
 	}
