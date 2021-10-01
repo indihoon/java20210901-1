bda6b9327f8ea79aae2f64e34c23306e839fb2bf
sebaek <sebaek@gmail.com>
2021-10-01 14:44:41 +0900

    client recieve text data

--- src/p18io/p03lecture/p07network/Ex05ClientGetText.java
+++ src/p18io/p03lecture/p07network/Ex05ClientGetText.java
@@ -1,5 +1,35 @@
 package p18io.p03lecture.p07network;
 
-public class Ex05ClientGetText {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
+public class Ex05ClientGetText {
+	public static void main(String[] args) throws Exception {
+		Socket socket = new Socket();
+		System.out.println("연결 시도 +++++");
+		socket.connect(new InetSocketAddress("172.30.1.18", 33333));
+		System.out.println("연결 성공 +++++");
+		
+		InputStream is = socket.getInputStream();
+		Reader rd = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(rd);
+		
+		// text데이터 받기
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			System.out.println("출력:" + line);
+		}
+		
+		br.close();
+		socket.close();
+	}
 }
+
+
+
+
+
