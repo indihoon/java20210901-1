5a2661c0f3494c9a304da66b8df489525667b5af
sebaek <sebaek@gmail.com>
2021-10-01 11:26:38 +0900

    sol byte to char

--- src/p18io/p02quiz/Ex15.java
+++ src/p18io/p02quiz/Ex15.java
@@ -2,11 +2,14 @@ package p18io.p02quiz;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Scanner;
 
 public class Ex15 {
 	public static void main(String[] args) throws Exception {
 		OutputStream os = getOutputStream("C:\\Users\\user\\Desktop\\iotest\\outtestI.txt");
+		Writer writer = new OutputStreamWriter(os);
 		Scanner scanner = new Scanner(System.in);
 		
 		String input = null;
@@ -17,10 +20,18 @@ public class Ex15 {
 			
 			// input을 outtestI.txt 파일에 쓰기
 			// exit를 입력받으면 loop 종료
+			
+			if (input.equals("exit")) {
+				break;
+			}
+			
+			writer.write(input);
+			writer.write("\n");
 		}
 		
 		System.out.println("종료");
 		scanner.close();
+		writer.close();
 		os.close();
 	}
 
