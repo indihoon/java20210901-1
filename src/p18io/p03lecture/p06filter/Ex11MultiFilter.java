d5fba7d80b573af9c76651a7c6ac200a616962fe
sebaek <sebaek@gmail.com>
2021-10-01 12:01:31 +0900

    sol byte to char

--- src/p18io/p03lecture/p06filter/Ex11MultiFilter.java
+++ src/p18io/p03lecture/p06filter/Ex11MultiFilter.java
@@ -1,16 +1,23 @@
 package p18io.p03lecture.p06filter;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Ex11MultiFilter {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String file = "C:\\Users\\user\\Desktop\\iotest\\outtestJ.txt";
 		
 		InputStream is = new FileInputStream(file);
 		// inputStream to Reader
 		// Reader를 BufferedReader로 감싸서 (BufferedReader의 변수 br)
 		
+		Reader rd = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(rd);
+		
+		String line = null;
 		// 한 줄씩 읽어서 출력
 		while ((line = br.readLine()) != null) {
 			System.out.println(line);
